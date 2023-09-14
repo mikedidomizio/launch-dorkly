198010d8795fbb04e731c083ef43a90201f46fbb
Mike DiDomizio <mikedidomizio@gmail.com>
2023-09-13 20:44:00 -0400

    Show comparison between two projects flags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,6 +40,14 @@ const Items = ({items} : any) => {
             <td>
                 {item.kind}
             </td>
+                <td>
+                    {Object.values(item.environments).map((environment: any) => {
+                        return <>
+                            <b>{environment._environmentName}</b>
+                            <b>{'' + environment.on}</b>
+                        </>
+                    })}
+                </td>
             </tr>
         })}
     </table>
@@ -57,6 +65,44 @@ const sortItems = (a: any, b: any) => {
     return 0
 }
 
+const flagsMatch = (item1: any, item2: any): boolean => {
+    return item1.name === item2.name && item1.kind === item2.kind
+}
+
+const valuesMatch = (item1Val: any, item2Val: any): boolean => {
+    return item1Val === item2Val
+}
+
+const ItemsProjects = ({ items1, items2}: any) => {
+
+    console.log(items2[0].environments)
+    // todo handle missing flags if one side is missing
+    return <table className="border-spacing-1.5">
+        <tr>
+            <th>Flag name</th>
+            <th>Name and Kind match</th>
+            <th colSpan={6}>Environments</th>
+        </tr>
+        {items1.map((item: any, index: number) => {
+            return <tr key={index}>
+                <td>
+                    {item.name}
+                </td>
+                <td>
+                    {'' + flagsMatch(item, items2[index])}
+                </td>
+                {Object.entries(item.environments).map(([ key, values ]: any) => {
+                    return <td key={key}>
+                        <b>{values._environmentName}</b>
+                        <br/>
+                        {valuesMatch(values.on, items2[0].environments[key].on) ? <>✅</> : <span title={`${values.on} - ${items2[0].environments[key].on}`}>❌</span>}
+                    </td>
+                })}
+            </tr>
+        })}
+    </table>
+}
+
 export default async function Home() {
   const project1 = await listFlagsProjectOne()
   const project2 = await listFlagsProjectTwo()
@@ -64,16 +110,7 @@ export default async function Home() {
   return (
     <main className="min-h-screen flex-col p-24">
         <div className="flex flex-row">
-            <div>
-                Project One
-
-                <Items items={project1.items.sort(sortItems)} />
-            </div>
-            <div>
-                Project Two
-
-                <Items items={project2.items.sort(sortItems)} />
-            </div>
+            <ItemsProjects items1={project1.items.sort(sortItems)} items2={project2.items.sort(sortItems)} ></ItemsProjects>
         </div>
     </main>
   )
