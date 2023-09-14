9002a1cb6d3eec484f3f528d21f38785c7623524
Mike DiDomizio <mikedidomizio@gmail.com>
2023-09-14 19:44:51 -0400

    Fix removal of the expected env var (#2)

--- src/app/api/update-variation/route.ts
+++ src/app/api/update-variation/route.ts
@@ -5,14 +5,17 @@ export async function PATCH(req: Request) {
     return NextResponse.json({ error: 'Error' }, { status: 500 })
   }
 
-  const { featureFlagKey, variation, value } = await req.json()
+  const { featureFlagKey, project, variation, value } = await req.json()
 
-  if (variation !== 'onVariationValue' && variation !== 'offVariationValue') {
+  if (
+    !project ||
+    (variation !== 'onVariationValue' && variation !== 'offVariationValue')
+  ) {
     return NextResponse.json({ error: 'Bad request' }, { status: 400 })
   }
 
   const resp = await fetch(
-    `https://app.launchdarkly.com/api/v2/flags/${process.env.LD_PROJECT_TWO}/${featureFlagKey}?ignoreConflicts=true`,
+    `https://app.launchdarkly.com/api/v2/flags/${project}/${featureFlagKey}?ignoreConflicts=true`,
     {
       method: 'PATCH',
       headers: {
