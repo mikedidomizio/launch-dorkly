9afe0954474a2c58188b72745988767458ca7036
Mike DiDomizio <mikedidomizio@gmail.com>
2023-09-16 21:53:05 -0400

    Commit out of sync lockfile

---
+++ src/app/api/create-flag/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server'
+import { cookies } from 'next/headers'
+import { allSet } from '@/helpers/allSet'
+export async function POST(req: Request) {
+  const cookieStore = cookies()
+  const token = cookieStore.get('LD_TOKEN')
+
+  const reqParams = await req.json()
+
+  if (!allSet(reqParams) || !token) {
+    return NextResponse.json({ error: 'Bad request' }, { status: 400 })
+  }
+
+  const { projectKey } = reqParams
+
+  const resp = await fetch(
+    `https://app.launchdarkly.com/api/v2/flags/${projectKey}`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: token.value,
+      },
+      body: JSON.stringify(reqParams),
+    },
+  )
+
+  return NextResponse.json(resp)
+}
