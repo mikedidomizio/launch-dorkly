8770947436d939cfa83885731b75c57fa0cbc544
Mike DiDomizio <mikedidomizio@gmail.com>
2023-09-21 13:24:24 -0400

    Add test for dropdown redirect

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -19,20 +19,33 @@ test.use({
 })
 
 test.describe('project page', () => {
-  test('should list all the projects as links', async ({ page, context }) => {
-    page.on('console', (msg) => {
-      console.log(`Error text: "${msg.text()}"`)
-    })
-
+  test.beforeEach(async ({ page }, testInfo) => {
     await page.goto('http://localhost:3000')
 
     await page.getByPlaceholder('LaunchDarkly Access Token').type('1234567890')
     await page.getByRole('button', { name: 'Submit' }).click()
     await page.waitForURL('http://localhost:3000')
+  })
 
+  test('should list all the projects as links', async ({ page, context }) => {
     await expect(page.getByRole('link', { name: 'My Project' })).toBeVisible()
     await expect(
       page.getByRole('link', { name: 'My Second Project' }),
     ).toBeVisible()
+  })
+
+  test('should redirect the user to the copy page if both dropdowns selected', async ({
+    page,
+    context,
+  }) => {
+    const [firstDropdown, secondDropdown] = await page
+      .getByRole('combobox')
+      .all()
+    await firstDropdown.selectOption({ label: 'My Project' })
+    await secondDropdown.selectOption({ label: 'My Second Project' })
+
+    await page.waitForURL(
+      'http://localhost:3000/copy/my-project/to/my-second-project',
+    )
   })
 })
