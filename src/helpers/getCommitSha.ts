6a84d2766669217997b8b45ef436baf818c5ac19
Mike DiDomizio <mikedidomizio@gmail.com>
2023-10-29 11:26:48 -0400

    Access SHA through framework env var

--- src/helpers/getCommitSha.ts
+++ src/helpers/getCommitSha.ts
@@ -1,8 +1,8 @@
 export const getCommitSha = (): string => {
-  const { NEXT_PUBLIC_GIT_COMMIT_SHA } = process.env
+  const { NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA } = process.env
 
-  if (NEXT_PUBLIC_GIT_COMMIT_SHA) {
-    return NEXT_PUBLIC_GIT_COMMIT_SHA
+  if (NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA) {
+    return NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA
   }
 
   return 'Running locally or could not get SHA'
