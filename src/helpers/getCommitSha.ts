66e909297526bea0a8a5b35b16a93e5d272641ea
Mike DiDomizio <mikedidomizio@gmail.com>
2023-10-29 13:41:59 -0400

    Log env vars

--- src/helpers/getCommitSha.ts
+++ src/helpers/getCommitSha.ts
@@ -1,6 +1,8 @@
 export const getCommitSha = (): string => {
   const { NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA } = process.env
 
+  console.log(process.env)
+
   if (NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA) {
     return NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA
   }
