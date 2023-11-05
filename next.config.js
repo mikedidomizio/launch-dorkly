fef2b5fe6d71ec517120305d9577a0e1c0844199
Mike DiDomizio <mikedidomizio@gmail.com>
2023-11-05 16:48:17 -0500

    Add ESLint rule to catch .only in tests

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {},
+  eslint: {
+    dirs: ['src', 'tests'], // by default it won't check the tests directory
+  },
 }
 
 module.exports = nextConfig
