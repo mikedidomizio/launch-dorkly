5712c23e1b89ddb8011c11231ced6291f9ec1e6c
Mike DiDomizio <mikedidomizio@gmail.com>
2023-11-05 12:53:35 -0500

    Fix displaying server side data into client side component

--- src/app/api/listProjects.ts
+++ src/app/api/listProjects.ts
@@ -1,12 +1,22 @@
 import { cookies } from 'next/headers'
 import { LaunchDarklyPromise } from '@/app/api/launch-darkly-promise'
 import { ListProjects } from '@/types/listProjects.types'
+import { NextResponse } from 'next/server'
 
-const fetchFn = async (token: string): Promise<ListProjects> => {
-  return LaunchDarklyPromise<ListProjects>(token, 'ProjectsApi', 'getProjects')
+const fetchFn = async (token: string): Promise<NextResponse<ListProjects>> => {
+  const ldResponse = await LaunchDarklyPromise<ListProjects>(
+    token,
+    'ProjectsApi',
+    'getProjects',
+    '',
+  )
+
+  return NextResponse.json<ListProjects>(ldResponse)
 }
 
-export const listProjects = async (cookie?: string) => {
+export const listProjects = async (
+  cookie?: string,
+): Promise<NextResponse<ListProjects> | null> => {
   if (cookie) {
     return fetchFn(cookie)
   }
