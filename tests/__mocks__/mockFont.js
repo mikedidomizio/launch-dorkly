7fa7d0353b99dc64bd6ec7c3c6f6bc2c573391ac
Mike DiDomizio <mikedidomizio@gmail.com>
2023-11-06 22:23:56 -0500

    Fake @font-face to see if it passes through

--- tests/__mocks__/mockFont.js
+++ tests/__mocks__/mockFont.js
@@ -1,4 +1,9 @@
 module.exports = {
-  'https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap':
-    'body {',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap': `
+    @font-face {
+      font-family: 'test';
+      src: url('./test.woff') format('woff');
+    }
+    
+    body {`,
 }
