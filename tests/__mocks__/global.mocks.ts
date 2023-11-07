687682c8104af0e01179142accb3b94d714a50ee
Mike DiDomizio <mikedidomizio@gmail.com>
2023-11-06 22:15:17 -0500

    Mock using JS file

--- tests/__mocks__/global.mocks.ts
+++ tests/__mocks__/global.mocks.ts
@@ -1,5 +1,6 @@
 import { rest, test } from 'next/experimental/testmode/playwright/msw'
 
+// todo this does nothing currently, remove if mocking fonts works in CI
 const googleFontsHandler = [
   rest.get('https://fonts.googleapis.com/*', (req, res, ctx) => {
     return res(ctx.status(200), ctx.text(''))
