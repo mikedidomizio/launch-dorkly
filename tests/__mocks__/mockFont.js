86bd7fb4219069587416a3b2ce110650c25c7401
Mike DiDomizio <mikedidomizio@gmail.com>
2023-11-06 22:45:52 -0500

    Silence Next errors in CI when can't load font without previous build (#40)
    Fix error by using NEXT_FONT_GOOGLE_MOCKED_RESPONSES

---
+++ tests/__mocks__/mockFont.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap': `
+    @font-face {
+      font-family: 'Open Sans';
+      font-style: normal;
+      font-weight: 300 800;
+      font-stretch: 100%;
+      font-display: swap;
+      src: url(https://fonts.gstatic.com/s/opensans/v34/memvYaGs126MiZpBA-UvWbX2vVnXBbObj2OVTSKmu0SC55K5gw.woff2) format('woff2');
+      unicode-range: U+0460-052F, U+1C80-1C88, U+20B4, U+2DE0-2DFF, U+A640-A69F, U+FE2E-FE2F;
+    }
+
+    body { }`,
+}
