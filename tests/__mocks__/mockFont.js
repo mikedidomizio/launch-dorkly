2f8a0db081a368e59258e3ffff5b8325653eb527
Mike DiDomizio <mikedidomizio@gmail.com>
2023-11-06 22:18:23 -0500

    Get rid of body split error

--- tests/__mocks__/mockFont.js
+++ tests/__mocks__/mockFont.js
@@ -1,3 +1,4 @@
 module.exports = {
-    "https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap": true
+  'https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap':
+    'body {',
 }
